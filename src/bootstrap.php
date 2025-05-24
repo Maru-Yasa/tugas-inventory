7ebbad4ab1d1c52a6092d27df42d82d86a2c6a25
Maru-yasa <marufilyasa13@gmail.com>
2025-05-24 18:07:53 +0700

    fix: enable simple auth

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,15 +9,15 @@ require __DIR__ . '/../vendor/autoload.php';
 define('ROOT_PATH', dirname(__DIR__) . '/src/');
 
 if (php_sapi_name() != 'cli') {
-    // if (
-    //     !isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
-    //     $_SERVER['PHP_AUTH_USER'] !== $valid_username || $_SERVER['PHP_AUTH_PW'] !== $valid_password
-    // ) {
-    //     header('WWW-Authenticate: Basic realm="Restricted Area"');
-    //     header('HTTP/1.0 401 Unauthorized');
-    //     echo 'Unauthorized';
-    //     exit;
-    // }
+    if (
+        !isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
+        $_SERVER['PHP_AUTH_USER'] !== $valid_username || $_SERVER['PHP_AUTH_PW'] !== $valid_password
+    ) {
+        header('WWW-Authenticate: Basic realm="Restricted Area"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Unauthorized';
+        exit;
+    }
     $router = require ROOT_PATH . '/route.php';
 }
 
